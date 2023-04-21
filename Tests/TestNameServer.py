633fd865cef0bf5ee071f3f8b46e923d0bc64a17
Ziang Tong <97473223+DannyTDS@users.noreply.github.com>
2023-04-21 16:12:59 -0400

    test program clean up

--- Tests/TestNameServer.py
+++ Tests/TestNameServer.py
@@ -9,34 +9,34 @@ class Client:
         server_port = int(server_port)
         self.server_addr = (server_host, server_port)
     
-    def __del__(self):
-        self.socket.close()
-    
     def new_socket(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect(self.server_addr)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(self.server_addr)
+        return s
     
-    def recv(self):
-        sz = self.socket.recv(8)
+    def recv(self, s):
+        sz = s.recv(8)
         length = int.from_bytes(sz, "big")
         msg = b''
         while len(msg) < length:
             to_read = length - len(msg)
-            msg += self.socket.recv(1024 if to_read > 1024 else to_read)
+            msg += s.recv(1024 if to_read > 1024 else to_read)
         return json.loads(msg.decode())
 
     def register(self, name, status='online') -> bool:
-        self.new_socket()
+        s = self.new_socket()
         msg = {'op': 'register', 'name': name, 'address': ':'.join([str(x) for x in self.socket.getsockname()]), 'status': status}
-        self.socket.send(len(json.dumps(msg)).to_bytes(8, 'big') + json.dumps(msg).encode())
-        res = self.recv()
+        s.send(len(json.dumps(msg)).to_bytes(8, 'big') + json.dumps(msg).encode())
+        res = self.recv(s)
+        s.close()
         return res['status'] == 'ok'
     
     def lookup(self, name):
-        self.new_socket()
+        s = self.new_socket()
         msg = {'op': 'lookup', 'name': name}
-        self.socket.send(len(json.dumps(msg)).to_bytes(8, 'big') + json.dumps(msg).encode())
-        res = self.recv()
+        s.send(len(json.dumps(msg)).to_bytes(8, 'big') + json.dumps(msg).encode())
+        res = self.recv(s)
+        s.close()
         return res if res['status'] != 'error' else None
 
 
